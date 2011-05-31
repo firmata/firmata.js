c531256c8c1d5ede90660a74f986aee4d8e8b7fe
Julian Gautier <julian.gautier@alumni.neumont.edu>
2011-05-30 20:31:29 -0700

    added repl

---
+++ repl.js
@@ -0,0 +1,13 @@
+var firmata=require('./lib/firmata.js')
+   ,repl = require('repl');
+console.log('Enter USB Port and press enter:');
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.once('data',function(chunk){
+    var port = chunk.replace('\n','');
+    console.log('port');
+    var board =  new firmata.Board(port,function(){
+        console.log('Successfully Connected to ')+port;
+        repl.start('firmata>').context.board=board;
+    });
+});
