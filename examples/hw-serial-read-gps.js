66bfdcf3c881df3e8b621f682eb5ba18aba8676a
Jeff Hoefs <jeff.hoefs@gmail.com>
2015-10-03 15:22:48 -0700

    move bytesToRead from serialConfig to serialRead
    removed old serial examples

--- examples/hw-serial-read-gps.js
+++ examples/hw-serial-read-gps.js
@@ -5,17 +5,24 @@ board.on("ready", function() {
   console.log("READY");
 
   var HW_SERIAL1 = board.SERIAL_PORT_IDs.HW_SERIAL1;
+  var maxBytesToRead = 4;
 
   board.serialConfig({
     portId: HW_SERIAL1,
-    baud: 9600,
-    //bytesToRead: 1
+    baud: 9600
   });
 
+  // leave 2nd parameter (maxBytesToRead) to read all available bytes in buffer
+  // board.serialRead(HW_SERIAL1, maxBytesToRead, function(data) {
   board.serialRead(HW_SERIAL1, function(data) {
     console.log(new Buffer(data).toString("ascii"));
   });
 
+  board.on("string", function (message) {
+    console.log(message);
+  });
+
+  // log serial pin numbers
   for (var pin in board.pins) {
     var modes = board.pins[pin].supportedModes;
     for (var mode in modes) {
