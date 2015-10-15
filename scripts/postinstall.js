a75b4f5f276d0bf2fa6407ae307a753ad9d50759
Rick Waldron <waldron.rick@gmail.com>
2015-10-15 13:01:31 -0400

    Conditional serialport version installation.
    Here's a run through:
    
    ```
    rwaldron at nova in ~/clonez/firmata on master*
    $ node -v
    v4.2.1
    rwaldron at nova in ~/clonez/firmata on master*
    $ node scripts/postinstall.js
    CXX(target) Release/obj.target/serialport/src/serialport.o
    CXX(target) Release/obj.target/serialport/src/serialport_unix.o
    CXX(target) Release/obj.target/serialport/src/serialport_poller.o
    SOLINK_MODULE(target) Release/serialport.node
    SOLINK_MODULE(target) Release/serialport.node: Finished
    COPY /Users/rwaldron/clonez/firmata/node_modules/serialport/build/serialport/v2.0.1/Release/node-v46-darwin-x64/serialport.node
    TOUCH Release/obj.target/action_after_build.stamp
    serialport@2.0.1 node_modules/serialport
    serialport@2.0.1 installed.
    rwaldron at nova in ~/clonez/firmata on master*
    $ nvm use 0.12.4
    Now using node v0.12.4
    rwaldron at nova in ~/clonez/firmata on master*
    $ node -v
    v0.12.4
    rwaldron at nova in ~/clonez/firmata on master*
    $ node scripts/postinstall.js
    [serialport] Success: "/Users/rwaldron/clonez/firmata/node_modules/serialport/build/serialport/v1.7.4/Release/node-v14-darwin-x64/serialport.node" is installed via remote
    serialport@1.7.4 node_modules/serialport
    serialport@1.7.4 installed.
    rwaldron at nova in ~/clonez/firmata on master*
    $ nvm use 0.10.32
    Now using node v0.10.32
    rwaldron at nova in ~/clonez/firmata on master*
    $ node -v
    v0.10.32
    rwaldron at nova in ~/clonez/firmata on master*
    $ node scripts/postinstall.js
    [serialport] Success: "/Users/rwaldron/clonez/firmata/node_modules/serialport/build/serialport/v1.7.4/Release/node-v11-darwin-x64/serialport.node" is installed via remote
    serialport@1.7.4 node_modules/serialport
    serialport@1.7.4 installed.
    rwaldron at nova in ~/clonez/firmata on master*
    $ nvm deactivate
    /Users/rwaldron/.nvm/*/bin removed from $PATH
    /Users/rwaldron/.nvm/*/share/man removed from $MANPATH
    rwaldron at nova in ~/clonez/firmata on master*
    $ node -v
    v4.2.1
    rwaldron at nova in ~/clonez/firmata on master*
    $ node scripts/postinstall.js
    CXX(target) Release/obj.target/serialport/src/serialport.o
    CXX(target) Release/obj.target/serialport/src/serialport_unix.o
    CXX(target) Release/obj.target/serialport/src/serialport_poller.o
    SOLINK_MODULE(target) Release/serialport.node
    SOLINK_MODULE(target) Release/serialport.node: Finished
    COPY /Users/rwaldron/clonez/firmata/node_modules/serialport/build/serialport/v2.0.1/Release/node-v46-darwin-x64/serialport.node
    TOUCH Release/obj.target/action_after_build.stamp
    serialport@2.0.1 node_modules/serialport
    serialport@2.0.1 installed.
    ```

---
+++ scripts/postinstall.js
@@ -0,0 +1,45 @@
+require("es6-shim");
+
+var semver = require("semver");
+var spawn = require("child_process").spawn;
+var sp = {
+  version: "1.7.4",
+  get atVersion() {
+    return "serialport@" + sp.version;
+  },
+};
+
+if (semver.gte(process.version, "4.2.1")) {
+  sp.version = "latest";
+}
+
+sp.atVersion = "serialport@" + sp.version;
+
+var npm = spawn("npm", ["install", sp.atVersion]);
+
+npm.stdout.on("data", function(data) {
+  var received = data.toString("ascii").split("\n")[0].trim();
+  var matches;
+
+  if (received) {
+    console.log(received);
+
+    if (received.includes("serialport@")) {
+      matches = received.match(/@(.*) /);
+
+      if (matches.length) {
+        sp.version = matches[1];
+      }
+    }
+  }
+});
+
+npm.on("close", function(code) {
+  var result;
+  if (code !== 0) {
+    result = "installation failed. Error Code: " + code;
+  } else {
+    result = "installed.";
+  }
+  console.log(sp.atVersion, result);
+});
