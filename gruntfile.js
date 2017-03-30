c3435c3cc4eb17530f34cd28e505545ef871c145
Rick Waldron <waldron.rick@gmail.com>
2017-03-30 11:30:57 -0400

    Gruntfile.js: adds changelog task
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,6 @@
+var cp = require("child_process");
+var tags = require("common-tags");
+
 module.exports = function (grunt) {
   grunt.initConfig({
     mochaTest: {
@@ -108,4 +111,66 @@ module.exports = function (grunt) {
     grunt.task.run("mochaTest");
   });
 
+  grunt.registerTask("changelog", "'changelog', 'changelog:v0.0.0..v0.0.2' or 'changelog:v0.0.0'", (arg) => {
+    var done = grunt.task.current.async();
+    var tags = cp.execSync("git tag --sort version:refname").toString().split("\n");
+    var tagIndex = -1;
+    var range;
+    var revisionRange;
+
+    if (!arg) {
+      // grunt changelog
+      range = tags.filter(Boolean).slice(-2);
+    } else {
+      if (arg.includes("..")) {
+        // grunt changelog:<revision-range>
+        if (!arg.startsWith("v") || !arg.includes("..v")) {
+          range = arg.split("..").map(tag => tag.startsWith("v") ? tag : `v${tag}`);
+        } else {
+          // arg is a well formed <revision-range>
+          revisionRange = arg;
+        }
+      } else {
+        // grunt changelog:<revision>
+        if (!arg.startsWith("v")) {
+          arg = `v${arg}`;
+        }
+
+        tagIndex = tags.indexOf(arg);
+        range = [tags[tagIndex - 1], tags[tagIndex]];
+      }
+    }
+
+    if (!range && revisionRange) {
+      range = revisionRange.split("..");
+    }
+
+    if (!revisionRange && (range && range.length)) {
+      revisionRange = `${range[0]}..${range[1]}`;
+    }
+
+    cp.exec(`git log --format='|%h|%s|' ${revisionRange}`, (error, result) => {
+      if (error) {
+        console.log(error.message);
+        return;
+      }
+
+      var rows = result.split("\n").filter(commit => {
+        return !commit.includes("|Merge ") && !commit.includes(range[0]);
+      });
+
+      // Extra whitespace above and below makes it easier to quickly copy/paste from terminal
+      grunt.log.writeln(`\n\n${changelog(rows)}\n\n`);
+
+      done();
+    });
+  });
 };
+
+function changelog(rows) {
+  return tags.stripIndent `
+| Commit | Message/Description |
+| ------ | ------------------- |
+${rows.join("\n")}
+`;
+}
