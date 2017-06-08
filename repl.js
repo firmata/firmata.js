3c33acff288f1888d7cc97ec89ad9a225fc55c66
Brad Buchanan <bradley.c.buchanan@gmail.com>
2017-06-08 15:21:23 -0700

    Use firmata correctly in repl.js

--- repl.js
+++ repl.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-var firmata = require('./lib/firmata.js'),
+var Board = require('./lib/firmata.js'),
     repl = require('repl');
 console.log('Enter USB Port and press enter:');
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 process.stdin.once('data', function(chunk) {
     var port = chunk.replace('\n', '');
-    var board = new firmata.Board(port, function() {
+    var board = new Board(port, function() {
         console.log('Successfully Connected to ' + port);
         repl.start('firmata>').context.board = board;
     });
